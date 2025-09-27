92aa5f117814776e408637dc316644b51b16fb4a
DiegoJsH <diego.palominos2006@gmail.com>
2025-09-27 18:51:14 -0500

    url api render

--- api-service.js
+++ api-service.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://springbootpsicoclinic.onrender.com';
 
 // API Service simplificado para Pacientes
 class PacienteService {
